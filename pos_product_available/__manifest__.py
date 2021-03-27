32be512b71d956b6094b23d6fbfb2c277fd2eb15
unknown <raulerueda@gmail.com>
2021-03-27 11:43:58 -0500

    Pos Disponible2

---
+++ pos_product_available/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Available quantity of products in POS",
+    "images": ["images/pos_product_available.jpg"],
+    "version": "14.0.1.0.6",
+    "author": "IT-Projects LLC, Ivan Yelizariev",
+    "license": "Other OSI approved licence",  # MIT
+    "category": "Point Of Sale",
+    "support": "pos@it-projects.info",
+    "website": "https://twitter.com/yelizariev",
+    "depends": ["point_of_sale", "stock"],
+    "data": ["data.xml"],
+    "qweb": ["static/src/xml/pos.xml"],
+    "installable": True,
+}
